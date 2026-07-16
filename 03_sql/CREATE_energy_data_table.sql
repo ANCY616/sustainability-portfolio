d50c6d8d48ac452d4d7e756e89f2ac47a134fa17
ANCY616 <74260028+ANCY616@users.noreply.github.com>
2026-07-16 15:27:50 +0530

    Update CREATE_energy_data_table.sql

--- 03_sql/CREATE_energy_data_table.sql
+++ 03_sql/CREATE_energy_data_table.sql
@@ -19,23 +19,23 @@ CREATE TABLE energy_data (
     HVAC_kWh     INT,
     Lighting_kWh INT,
     Equipment_kWh INT,
-    CO2_kg       INT,
+    Emissions_kgCO2e DECIMAL(10,1),
     Month_Num    INT
 );
 
 -- Step 3: Insert all 12 months of data
-INSERT INTO energy_data VALUES ('Jan', 5820, 3370, 1240, 1210, 2328, 1);
-INSERT INTO energy_data VALUES ('Feb', 5610, 3180, 1220, 1210, 2244, 2);
-INSERT INTO energy_data VALUES ('Mar', 5190, 2980, 1130, 1080, 2076, 3);
-INSERT INTO energy_data VALUES ('Apr', 4980, 2760, 1110, 1110, 1992, 4);
-INSERT INTO energy_data VALUES ('May', 5530, 3260, 1160, 1110, 2212, 5);
-INSERT INTO energy_data VALUES ('Jun', 6480, 3980, 1290, 1210, 2592, 6);
-INSERT INTO energy_data VALUES ('Jul', 7210, 4530, 1380, 1300, 2884, 7);
-INSERT INTO energy_data VALUES ('Aug', 6990, 4350, 1340, 1300, 2796, 8);
-INSERT INTO energy_data VALUES ('Sep', 6250, 3840, 1210, 1200, 2500, 9);
-INSERT INTO energy_data VALUES ('Oct', 5790, 3410, 1180, 1200, 2316, 10);
-INSERT INTO energy_data VALUES ('Nov', 5380, 3080, 1140, 1160, 2152, 11);
-INSERT INTO energy_data VALUES ('Dec', 5870, 3410, 1230, 1230, 2348, 12);
+INSERT INTO energy_data VALUES ('Jan', 5820, 3370, 1240, 1210, 2230.8, 1);
+INSERT INTO energy_data VALUES ('Feb', 5610, 3180, 1220, 1210, 2150.3, 2);
+INSERT INTO energy_data VALUES ('Mar', 5190, 2980, 1130, 1080, 1989.3, 3);
+INSERT INTO energy_data VALUES ('Apr', 4980, 2760, 1110, 1110, 1908.5, 4);
+INSERT INTO energy_data VALUES ('May', 5530, 3260, 1160, 1110, 2119.6, 5);
+INSERT INTO energy_data VALUES ('Jun', 6480, 3980, 1290, 1210, 2483.8, 6);
+INSERT INTO energy_data VALUES ('Jul', 7210, 4530, 1380, 1300, 2763.6, 7);
+INSERT INTO energy_data VALUES ('Aug', 6990, 4350, 1340, 1300, 2679.3, 8);
+INSERT INTO energy_data VALUES ('Sep', 6250, 3840, 1210, 1200, 2395.6, 9);
+INSERT INTO energy_data VALUES ('Oct', 5790, 3410, 1180, 1200, 2219.3, 10);
+INSERT INTO energy_data VALUES ('Nov', 5380, 3080, 1140, 1160, 2062.2, 11);
+INSERT INTO energy_data VALUES ('Dec', 5870, 3410, 1230, 1230, 2250.0, 12);
 
 -- Step 4: Confirm it worked — you should see all 12 rows
 SELECT * FROM energy_data;
